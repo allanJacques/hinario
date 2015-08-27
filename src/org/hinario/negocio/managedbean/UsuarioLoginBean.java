c0100cf13fe65b01eeee0f0b3ba20b501febd64c
allanJacques <allanjnofs@gmail.com>
2015-08-27 07:28:02 -0400

    fazendo corte da imagem do usuario

--- src/org/hinario/negocio/managedbean/UsuarioLoginBean.java
+++ src/org/hinario/negocio/managedbean/UsuarioLoginBean.java
@@ -1,12 +1,19 @@
 package org.hinario.negocio.managedbean;
 
+import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.Serializable;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+import javax.imageio.ImageIO;
+import javax.imageio.stream.FileImageOutputStream;
 import javax.servlet.http.HttpServletRequest;
 
 import org.hinario.dao.UsuarioDAO;
@@ -16,6 +23,7 @@ import org.hinario.model.Usuario;
 import org.hinario.util.CriptografiaSHA1Util;
 import org.primefaces.component.tabview.Tab;
 import org.primefaces.event.TabChangeEvent;
+import org.primefaces.model.CroppedImage;
 import org.primefaces.model.DefaultStreamedContent;
 import org.primefaces.model.StreamedContent;
 
@@ -31,11 +39,19 @@ public class UsuarioLoginBean extends ManagedBeanBase implements Serializable {
 	private Usuario usuario;
 	private final UsuarioDAO dao;
 	private Tab tab;
+	private CroppedImage imagemRecortada;
+	private final File path;
+	private File imagem;
+	private byte[] imagemAntiga;
 	private String novaSenha;
 	private String confirmeNovaSenha;
 
 	public UsuarioLoginBean() {
 		this.dao = new UsuarioDAO();
+		this.path = new File(new File(new File(this.getClass().getResource("UsuarioLoginBean.class").getPath()).getParentFile().getParentFile().getParentFile().getParentFile().getParentFile().getParentFile().getParentFile(), "resources"), "logins");
+		if (!path.exists()) {
+			this.path.mkdirs();
+		}
 	}
 
 	public boolean isLogado() {
@@ -49,11 +65,20 @@ public class UsuarioLoginBean extends ManagedBeanBase implements Serializable {
 		} else {
 			this.usuario = this.dao.valida(this.email, new CriptografiaSHA1Util().criptografar(this.senha));
 			// this.usuario = this.usuarioDAO.valida(this.email, this.senha);
+			this.dao.atualizar(this.usuario);
 			this.senha = null;
 			if (this.usuario == null) {
 				FacesContext fc = FacesContext.getCurrentInstance();
 				fc.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "", this.appMessage.getString("message.senhaIncorreta")));
 			} else {
+				try {
+					this.imagem = new File(this.path, this.usuario.getIrmao().getNome());
+					FileOutputStream saida = new FileOutputStream(imagem);
+					saida.write(this.usuario.getImagem());
+					saida.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 				return "home?faces-redirect=true";
 			}
 		}
@@ -165,7 +190,39 @@ public class UsuarioLoginBean extends ManagedBeanBase implements Serializable {
 	}
 
 	public StreamedContent getImageUsuario() {
+		this.dao.atualizar(this.usuario);
 		return new DefaultStreamedContent(new ByteArrayInputStream(this.usuario.getImagem()));
+	}
+
+	public CroppedImage getImagemRecortada() {
+		return imagemRecortada;
+	}
+
+	public void setImagemRecortada(CroppedImage imagemRecortada) {
+		this.imagemRecortada = imagemRecortada;
+	}
+
+	public void cortar() {
+
+		FileImageOutputStream imageOutput;
+		try {
+			imageOutput = new FileImageOutputStream(this.imagem);
+			imageOutput.write(this.imagemRecortada.getBytes(), 0, this.imagemRecortada.getBytes().length);
+			imageOutput.close();
+
+			BufferedImage bufImgOrg = ImageIO.read(this.imagem);
+			BufferedImage bufImgRec = bufImgOrg.getSubimage(this.imagemRecortada.getLeft(), this.imagemRecortada.getTop(), this.imagemRecortada.getWidth(), this.imagemRecortada.getHeight());
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			ImageIO.write(bufImgRec, "jpg", baos);
+			baos.toByteArray();
+
+			this.imagemAntiga = this.usuario.getImagem();
+			this.usuario.setImagem(baos.toByteArray());
+			baos.close();
+		} catch (Exception e) {
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "Cropping failed."));
+		}
+
 	}
 
 }
